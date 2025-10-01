82befa03aed625776ac627839b366e7e59d84af6
Elena123-git <epabben544@g.educaand.es>
2025-10-01 19:03:57 +0200

    Subo tarea casa

---
+++ New python/ej2.py
@@ -0,0 +1,5 @@
+suma = 0
+for i in range(1,6):
+    suma = suma + i #esto es una variable en bucle (0+1=1,1+2=3,3+3=6,6+4=10 y 10+5=15)
+    print("Acumulado", suma, "sumando", i)
+print ("La suma total es:", suma)
