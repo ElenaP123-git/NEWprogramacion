f8f63337fd7158e9e37192e959cbeff65f3eb2d5
Elena123-git <epabben5442g.educaand.es>
2025-10-04 22:02:33 +0200

    Ejercicios bucles

---
+++ New python/ACTV_S/ej9.py
@@ -0,0 +1,5 @@
+# Imprimir cuadrados: Imprime el cuadrado de los números del 1 al 10.
+print("Numeros cuadrados del 1 al 10:")
+for numero in range(1, 11):
+    cuadrado = numero * numero
+    print("El cuadrado de", numero, "es", cuadrado)
