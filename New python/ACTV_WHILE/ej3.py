5c757cad6c03e91e96a7ae546156ce1c8e09de69
Elena123-git <epabben5442g.educaand.es>
2025-10-07 19:38:07 +0200

    Subo ejercicios while

---
+++ New python/ACTV_WHILE/ej3.py
@@ -0,0 +1,6 @@
+#Muestra los números del 10 al 1 en orden descendente con while
+contador = 10
+while contador >=1:
+    print(contador)
+    contador = contador - 1
+print("¡Fin del cconteo!")
---
+++ New python/ACTV_WHILE/ej4.py
@@ -0,0 +1,6 @@
+suma = 0 #para acumular los números en bucle
+N1 = int(input("Ingrese un número: "))
+while N1 != 0:
+    suma= suma + N1
+    N1 = int(input("Ingrese un número...: "))
+print("Total =",suma)
