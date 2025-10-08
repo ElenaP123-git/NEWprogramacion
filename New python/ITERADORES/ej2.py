1980b8368069302a27c54ccfcb8c3c4ea103e225
Elena123-git <epabben5442g.educaand.es>
2025-10-08 17:24:35 +0200

    Subo iteradores casa

--- New python/ITERADORES/ej2.py
+++ New python/ITERADORES/ej2.py
@@ -1,5 +1,17 @@
-num = int(input("Dame un número: "))
-while num >0:
-    estrella= "*" * num
+altura_maxima = int(input("Dame un número (altura del triángulo): "))
+
+# 'i' será la variable de control (empieza en la primera fila)
+i = 1
+
+print("--- Triángulo ---")
+while i <= altura_maxima:  # Condición: repetir mientras i no exceda su altura máxima
+    
+    # CORRECCIÓN: Usamos 'i' para dibujar los asteriscos (fila1, dibuja 1, fila 2, dbuja 2...).
+    estrella = "*" * i
+    i = i + 1 
     print(estrella)
     
+    # CORRECCIÓN: Incrementamos 'i' para que el dibujo crezca y el bucle termine.
+    
+    
+print("--- Fin del Dibujo ---")
